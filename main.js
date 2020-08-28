5eb9b004a99b9b50a1ce67a07b6a21df9f25af80
Valeria Curseri <curserivaleria@gmail.com>
2020-08-28 14:31:24 +0200

    File di base

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    // Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+
+    // Servendoci di handlebars stampiamo tutto a schermo.
+    
+});
