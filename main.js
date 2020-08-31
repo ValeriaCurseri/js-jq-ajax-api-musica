f9820dec0e52a792f7fbde159129dc6b2954a6c6
Valeria Curseri <curserivaleria@gmail.com>
2020-08-31 10:30:01 +0200

    Completo con valori con iniziali maiuscole

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ $(document).ready(function() {
             url: 'https://flynn.boolean.careers/exercises/api/array/music',     // API per avere a disposizione il contenuto
             method: "GET",
             success: function (risposta) {
-                for (var i = 0; i < risposta.response.length; i++){             // ciclo tutto l'array response
-                    var source = $("#entry-template").html();                   // collego handlebars
-                    var template = Handlebars.compile(source);
+                // OPZIONE 1
+                // for (var i = 0; i < risposta.response.length; i++){             // ciclo tutto l'array response
+                //     var source = $("#entry-template").html();                   // collego handlebars
+                //     var template = Handlebars.compile(source);
+                //
+                //     var context = risposta.response[i];
+                //     var html = template(context);
+                //
+                //     $('.cds-container').append(html);                           // append il template completato nel DOM
+                // }
 
-                    var context = risposta.response[i];
-                    var html = template(context);
-
-                    $('.cds-container').append(html);                           // append il template completato nel DOM
-                }
+                // OPZIONE 2 CON FZ
+                insertCd(risposta);
             },
             error: function(){
                 alert:('Ci sono stati degli errori');
@@ -26,21 +30,32 @@ $(document).ready(function() {
 
     // BONUS: tramite un filtro select permetto all'utente di filtrare i cd
 
-    $("select#generi").change(function(){                       // al cambiare del valore di select
+    $("select#generi").change(function(){                       // al cambiare del valore di select - FUNZIONA SU CHROME
+    // $('#generi option').click(function(){                    // al cambiare del valore di select - NON FUNZIONA SU CHROME
 
-        $('.cd').hide();                                        // 1. nascondo tutti i cd
-
-        var valore = $(this).val();                             // 2. memorizzo il valore dell'option cliccato
+        var valore = $(this).val();                             // 1. memorizzo il valore dell'option cliccato
         console.log(valore);
 
-        $('.cd').each(function(){                               // 3. ciclo tutti i cd
-            console.log('parte each');
-            if ($(this).hasClass(valore)){                      // SE il cd ciclato ha class = valore
-                $(this).show();                                 // lo mostro
-            } else {                                            // ALTRIMENTI
-                console.log('else');                            // non lo mostro
+        $('.cd').hide();                                        // 2. nascondo tutti i cd
+
+        // OPZIONE 1
+        // if (valore == tutti){                                // 3.1 SE il valore della scelta è 'tutti'
+        //     $('.cd').show();                                 // 3.2 mostro tutti i cd
+        // } else {                                             // 3.3 ALTRIMENTI
+        //     $('.cd.' + valore).show();                       // 3.4 mostro solo quelli con la classe uguale al valore
+        // }
+
+        // OPZIONE 2
+        $('.cd').each(function(){                               // 3.1 ciclo tutti i cd
+            if ($(this).hasClass(valore)){                      // 3.2 SE il cd ciclato ha class = valore
+                $(this).show();                                 // 3.3 lo mostro
+            } else if (valore == 'tutti') {                     // 3.4 SE INVECE il valore della scelta è uguale a tutti
+                $(this).show();                                 // 3.5 non lo mostro
             }
         })
+
+
+        // VECCHIO
 
         // $('#generi .genere:selected').each(function(){
         //
@@ -92,3 +107,18 @@ $(document).ready(function() {
     // });
 
 });
+
+
+// ---- funzioni ---- //
+
+function insertCd(data){
+    for (var i = 0; i < data.response.length; i++){             // ciclo tutto l'array response
+        var source = $("#entry-template").html();                   // collego handlebars
+        var template = Handlebars.compile(source);
+
+        var context = data.response[i];
+        var html = template(context);
+
+        $('.cds-container').append(html);                           // append il template completato nel DOM
+    }
+}
